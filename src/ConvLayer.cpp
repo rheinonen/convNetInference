466262a80379d2bca079fcf0d2a6fe4090d2d0c8
Blaize Berry <blaize@entefy.com>
2017-05-14 00:12:52 -0700

    Implement im2col, filterMatrix, cBlas dgemm (which won't currently work because it expects the matrices to be 1D arrays)

--- src/ConvLayer.cpp
+++ src/ConvLayer.cpp
@@ -1,25 +1,159 @@
+#include <cblas.h>
+
 #include "ConvLayer.h"
 
-ConvLayer::ConvLayer (int &shape, int stride, float &w, float &b) :
-  // weights and biases initialization are copy ops so lightweight
-  weights(w), biases(b), stride(stride)
+/**
+ * Constructor - Initialize layer params, read in weights, and calculate im2col shape for params.
+ */
+ConvLayer::ConvLayer (
+  const int* input_shape,
+  const int* kernel_shape,
+  const int* stride,
+  const int* padding,
+  const int* output_shape,
+  const float* weights,
+  const float* biases
+):
+  weights(weights),
+  biases(biases),
+  stride(stride),
+  input_shape(input_shape),
+  padding(padding),
+  kernel_shape(kernel_shape),
+  output_shape(output_shape)
 {
-  // shape will contain x and y dimensions of the convolutional filters in the
-  // zeroth and first positions respectively and the number of filters in the
-  // second position
-  fs_x = shape[0];
-  fs_y = shape[1];
-  depth = shape[2];
+  for (int i = 0; i < 2; i++) {
+    im2col_output_shape[i] = (this->input_shape[i] + 2 * this->padding[i] -
+      (this->dilation[i] * (this->kernel_shape[i] - 1) + 1)) / this->stride[i] + 1;
+  }
+};
+
+/**
+ * Basic im2col implementation - converts a 3D image tensor to matrix form
+ *
+ * @param input - the 3D image tensor to convert to matrix form
+ * @param output - an initalized 2D array to hold the output
+ */
+void ConvLayer::im2col(const float* input, float* output) {
+  int channel_size = this->input_shape[0] * this->input_shape[1];
+
+  for (int ch = 0; ch < this->input_shape[2]; ch++) {
+    for (int k_row = 0; k_row < this->kernel_shape[0]; k_row++) {
+      for (int k_col = 0; k_col < this->kernel_shape[1]; k_col++) {
+        int out_row = k_col + k_row * this->kernel_shape[1];
+
+        int in_row = -this->padding[0] + k_row * this->dilation[0];
+        for (in_row; in_row < this->input_shape[0] - this->padding[0]; in_row+=this->stride[0]) {
+          int in_col = -this->padding[1] + k_col * this->dilation[1];
+          for (in_col; in_col < this->input_shape[1] - this->padding[1]; in_col+=this->stride[1]) {
+            int out_col = in_col / this->stride[1] +
+              in_row * this->input_shape[1] / this->stride[0] + ch * channel_size;
+
+            if (in_row < 0 || in_row > this->input_shape[0] || in_row < 0 || in_row > this->input_shape[0]) {
+              output[out_row][out_col] = 0;
+            } else {
+              output[out_row][out_col] = input[in_row + k_row][in_col + k_col][ch];
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
-void ConvLayer::forwardProp(int dim_x, int dim_y, int n_channels, float &input) {
-  // implement me
+/**
+ * Basic col2im implementation - returns a 3D image tensor to its original form from matrix form
+ *
+ * @param input - matrix form of the image tensor
+ * @param output - an initalized 3D array to hold the output
+ */
+void ConvLayer::col2im(const float* input, float* output) {
+  // @TODO - implement me
+}
+
+/**
+ * Construct a matrix based on the filter weights
+ *
+ * @param fm - an initalized 2D array to hold the output
+ */
+void ConvLayer::filterMatrix(const float* fm) {
+  int n_rows = this->kernel_shape[2];
+  int n_cols = this->im2col_output_shape[0];
+
+  for (int filt = 0; filt < this->output_shape[2]; filt++) {
+    for (int ch = 0; ch < this->input_shape[2], ch++) {
+      for (int w_row = 0; w_row < this->kernel_shape[0]; w_row++) {
+        for (int w_col = 0; w_col < this->kernel_shape[1]; w_col++) {
+          fm[filt][ch * w_row * w_col] = this->weights[w_row][w_col][ch][filt];
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Serially perform forward propagation for the layer. Convert the image tensor to a matrix,
+ * create a matrix out of the convolutional filters, multiply these two matrices, and then
+ * optionally convert back to 3D image tensor form (not necessary if this layer is followed
+ * by a pooling layer)
+ *
+ * @param fm - an initalized 2D array to hold the output
+ */
+void ConvLayer::forwardProp(const float* input, float* result, bool perform_col2im=true) {
+  if (this->output_shape.size() != 3)
+    throw "ForwardProp output must be of dimension 3.";
+
+  float im_matrix[this->im2col_output_shape[0]][this->im2col_output_shape[1]];
+  this->im2col(input, im_matrix);
+
+  float flt_matrix[this->kernel_shape[2]][this->im2col_output_shape[0]];
+  this->filterMatrix(flt_matrix);
+
+  if (perform_col2im) {
+    float mat_prod[this->kernel_shape[2]][this->im2col_output_shape[1]] = {this->biases};
+    cblas_dgemm(
+      CblasRowMajor,
+      CblasNoTrans,
+      CblasNoTrans,
+      this->kernel_shape[2],
+      this->im2col_output_shape[1],
+      this->im2col_output_shape[0],
+      1.0,
+      flt_matrix,
+      this->kernel_shape[2],
+      im_matrix,
+      this->im2col_output_shape[0],
+      1.0,
+      mat_prod,
+      this->kernel_shape[2]
+    );
+
+    this->col2im(mat_prod, result);
+  } else {
+    for (int row=0; row < this->kernel_shape[2]; row++) {
+      for (int col=0; col < this->im2col_output_shape[1]; col++) {
+        result[row][col] = biases[row];
+      }
+    }
+    cblas_dgemm(
+      CblasRowMajor,
+      CblasNoTrans,
+      CblasNoTrans,
+      this->kernel_shape[2],
+      this->im2col_output_shape[1],
+      this->im2col_output_shape[0],
+      1.0,
+      flt_matrix,
+      this->kernel_shape[2],
+      im_matrix,
+      this->im2col_output_shape[0],
+      1.0,
+      result,
+      this->kernel_shape[2]
+    );
+  }
 }
 
 void ConvLayer::forwardPropThreaded(int dim_x, int dim_y, int n_channels, float &input) {
-  // implement me
-}
-
-void ConvLayer::forwardPropGPU(int dim_x, int dim_y, int n_channels, float &input) {
-  // implement me
+  // @TODO: implement me
 }
--- src/ConvLayer.h
+++ src/ConvLayer.h
@@ -7,14 +7,28 @@ namespace Layers {
 
 class ConvLayer {
 private:
-  unsigned int fs_x;
-  unsigned int fs_y;
-  unsigned int stride;
-  unsigned int depth;
+  int* input_shape;
+  int* kernel_shape, stride, padding;
+  int* output_shape, im2col_output_shape;
+
   float* weights;
   float* biases;
+
+  void im2col(const float* input, float* output) {};
+
+  void col2im(const float* input, float* output) {};
+
+  void filterMatrix(const float* fm) {};
 public:
-  ConvLayer (int &shape, int stride, float &w, float &b) {};
+  ConvLayer (
+    const int* input_shape,
+    const int* kernel_shape,
+    const int* stride,
+    const int* padding,
+    const int* output_shape,
+    const float* weights,
+    const float* biases
+  ) {};
 
   void forwardProp(int dim_x, int dim_y, int n_channels, float &input) {};
 
